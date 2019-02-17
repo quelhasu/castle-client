3a451583de100050d2a69d8cb888c1de177e6c90
quelhasu <uquelhas@gmail.com>
2019-02-17 21:22:57 +0100

    🧭 navbar update

--- components/Header.js
+++ components/Header.js
@@ -51,22 +51,9 @@ const Header = () => (
       </NavbarBrand>
         <Nav className="ml-auto" navbar>
           <NavItem>
-            <NavLink href="/components/">Components</NavLink>
+            <NavLink href="https://github.com/quelhasu/castle-client">GitHub <i class="fab fa-github-alt"></i></NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-              Options
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>Option 1</DropdownItem>
-              <DropdownItem>Option 2</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
+
         </Nav>
     </Navbar>
 
